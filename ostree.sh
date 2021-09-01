db48122bf32d6f94d4967ce513654aeb03d8374d
Xiaofeng Wang <henrywangxf@me.com>
2021-09-01 12:34:28 +0800

    Some fixes:
    1. Use rhel-85.json as filename
    2. OCP4 feature is for rhel 8.5 only
    3. Add ansible repo in rhel 8.5 switch branch in ostree.sh
    4. edge-container exposed port is 8080 for rhel 8.5 only
    5. Add rhel 8.5 scenario in bare metal testing
    6. Add trailling slash bug fix test on rhel 8.5 only

--- ostree.sh
+++ ostree.sh
@@ -67,10 +67,10 @@ case "${ID}-${VERSION_ID}" in
         OSTREE_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8-unknown"
         USER_IN_COMMIT="true"
+        sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
         BOOT_LOCATION="http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8/latest-RHEL-8.5.0/compose/BaseOS/x86_64/os/"
         CUT_DIRS=8
-        sudo cp files/rhel-8-5-0.json /etc/osbuild-composer/repositories/rhel-8-beta.json
-        sudo ln -sf /etc/osbuild-composer/repositories/rhel-8-beta.json /etc/osbuild-composer/repositories/rhel-8.json;;
+        sudo cp files/rhel-8-5-0.json /etc/osbuild-composer/repositories/rhel-85.json;;
     *)
         echo "unsupported distro: ${ID}-${VERSION_ID}"
         exit 1;;
