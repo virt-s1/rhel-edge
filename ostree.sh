ba75e2743c4f1b503e372a390331d2f6d7b86f4b
Xiaofeng Wang <henrywangxf@me.com>
2021-10-29 09:57:46 +0800

    Add RHEL 8.6 support and update RHEL 8.5 repo URL since it's GA

--- ostree.sh
+++ ostree.sh
@@ -68,7 +68,7 @@ case "${ID}-${VERSION_ID}" in
         OS_VARIANT="rhel8-unknown"
         USER_IN_COMMIT="true"
         sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-        BOOT_LOCATION="http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8/latest-RHEL-8.5.0/compose/BaseOS/x86_64/os/"
+        BOOT_LOCATION="http://download-node-02.eng.bos.redhat.com/rhel-8/rel-eng/RHEL-8/latest-RHEL-8.5.0/compose/BaseOS/x86_64/os/"
         CUT_DIRS=8
         sudo cp files/rhel-8-5-0.json /etc/osbuild-composer/repositories/rhel-85.json;;
     "rhel-8.6")
