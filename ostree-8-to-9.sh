e46dc360b2d2b4eef14e70ec3d9d3cb1ddeb85c2
Xiaofeng Wang <henrywangxf@me.com>
2022-03-16 17:41:03 +0800

    Update ostree-8-to-9.sh to use new ostree repo on ocp-c1

--- ostree-8-to-9.sh
+++ ostree-8-to-9.sh
@@ -39,14 +39,14 @@ case "${ID}-${VERSION_ID}" in
         PARENT_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8-unknown"
         BOOT_LOCATION="http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8/latest-RHEL-8.6.0/compose/BaseOS/x86_64/os/"
-        PARENT_REPO_URL_8="http://edge-rhel-8-6-rt-virt-qe-3rd.apps.ocp4.prod.psi.redhat.com/repo/"
+        PARENT_REPO_URL_8="http://edge-rhel-8-6-rt-rhel-edge.apps.ocp-c1.prod.psi.redhat.com/repo/"
         ;;
     "centos-9")
         OSTREE_REF="centos/9/${ARCH}/edge"
         PARENT_REF="centos/8/${ARCH}/edge"
         OS_VARIANT="centos-stream8"
         BOOT_LOCATION="http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/"
-        PARENT_REPO_URL_8="http://edge-cs8-normal-virt-qe-3rd.apps.ocp4.prod.psi.redhat.com/repo/"
+        PARENT_REPO_URL_8="http://edge-cs8-normal-rhel-edge.apps.ocp-c1.prod.psi.redhat.com/repo/"
         ;;
     *)
         echo "unsupported distro: ${ID}-${VERSION_ID}"
