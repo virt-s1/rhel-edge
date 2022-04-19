b33dae4929d5233d88cb95e4ec3b6ff0a2744ced
Xiaofeng Wang <henrywangxf@me.com>
2022-04-19 20:20:11 +0800

    Use cross build to build RHEL 8 edge container image on RHEL 9
    All steps are able to be run on the same VM

--- ostree-8-to-9.sh
+++ ostree-8-to-9.sh
@@ -38,22 +38,22 @@ case "${ID}-${VERSION_ID}" in
         OSTREE_REF="rhel/9/${ARCH}/edge"
         PARENT_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8-unknown"
+        DISTRO="rhel-86"
         BOOT_LOCATION="http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8/latest-RHEL-8.6.0/compose/BaseOS/x86_64/os/"
-        PARENT_REPO_URL_8="http://edge-rhel-8-6-rt-rhel-edge.apps.ocp-c1.prod.psi.redhat.com/repo/"
         ;;
     "rhel-9.1")
         OSTREE_REF="rhel/9/${ARCH}/edge"
         PARENT_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8-unknown"
+        DISTRO="rhel-87"
         BOOT_LOCATION="http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8/latest-RHEL-8.7.0/compose/BaseOS/x86_64/os/"
-        PARENT_REPO_URL_8="http://edge-rhel-8-7-rt-rhel-edge.apps.ocp-c1.prod.psi.redhat.com/repo/"
         ;;
     "centos-9")
         OSTREE_REF="centos/9/${ARCH}/edge"
         PARENT_REF="centos/8/${ARCH}/edge"
         OS_VARIANT="centos-stream8"
+        DISTRO="centos-8"
         BOOT_LOCATION="http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/"
-        PARENT_REPO_URL_8="http://edge-cs8-normal-rhel-edge.apps.ocp-c1.prod.psi.redhat.com/repo/"
         ;;
     *)
         echo "unsupported distro: ${ID}-${VERSION_ID}"
@@ -95,6 +95,7 @@ get_compose_metadata () {
 build_image() {
     blueprint_name=$1
     image_type=$2
+    ref=$3
 
     # Get worker unit file so we can watch the journal.
     WORKER_UNIT=$(sudo systemctl list-units | grep -o -E "osbuild.*worker.*\.socket")
@@ -103,7 +104,16 @@ build_image() {
 
     # Start the compose.
     greenprint "🚀 Starting compose"
-    sudo composer-cli --json compose start-ostree --parent "$PARENT_REF" --url "$PROD_REPO_URL" --ref "$OSTREE_REF" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
+    if [ $# -eq 4 ]; then
+        repo_url=$4
+        sudo composer-cli --json compose start-ostree --ref "$ref" --url "$repo_url" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
+    elif [ $# -eq 5 ]; then
+        repo_url=$4
+        parent_ref=$5
+        sudo composer-cli --json compose start-ostree --ref "$ref" --parent "$parent_ref" --url "$repo_url" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
+    else
+        sudo composer-cli --json compose start-ostree --ref "$ref" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
+    fi
     COMPOSE_ID=$(jq -r '.body.build_id' "$COMPOSE_START")
 
     # Wait for the compose to finish.
@@ -194,12 +204,87 @@ greenprint "Prepare ostree prod repo and configure stage repo"
 sudo rm -rf "$PROD_REPO"
 sudo mkdir -p "$PROD_REPO"
 sudo ostree --repo="$PROD_REPO" init --mode=archive
-sudo ostree --repo="$PROD_REPO" remote add --no-gpg-verify edge-stage-8 "$PARENT_REPO_URL_8"
 sudo ostree --repo="$PROD_REPO" remote add --no-gpg-verify edge-stage "$STAGE_REPO_URL"
+
+###########################################################
+##
+## rhel-edge container image for building installer image
+##
+###########################################################
+
+# Write a blueprint for ostree image.
+tee "$BLUEPRINT_FILE" > /dev/null << EOF
+name = "container"
+description = "A base rhel-edge container image"
+version = "0.0.1"
+distro = "$DISTRO"
+modules = []
+groups = []
+
+[[packages]]
+name = "python3"
+version = "*"
+
+[[customizations.user]]
+name = "admin"
+description = "Administrator account"
+password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHBKnB2FBXGQ/OkwZQfW/76ktHd0NX5nls2LPxPuUdl."
+key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzxo5dEcS+LDK/OFAfHo6740EyoDM8aYaCkBala0FnWfMMTOq7PQe04ahB0eFLS3IlQtK5bpgzxBdFGVqF6uT5z4hhaPjQec0G3+BD5Pxo6V+SxShKZo+ZNGU3HVrF9p2V7QH0YFQj5B8F6AicA3fYh2BVUFECTPuMpy5A52ufWu0r4xOFmbU7SIhRQRAQz2u4yjXqBsrpYptAvyzzoN4gjUhNnwOHSPsvFpWoBFkWmqn0ytgHg3Vv9DlHW+45P02QH1UFedXR2MqLnwRI30qqtaOkVS+9rE/dhnR+XPpHHG+hv2TgMDAuQ3IK7Ab5m/yCbN73cxFifH4LST0vVG3Jx45xn+GTeHHhfkAfBSCtya6191jixbqyovpRunCBKexI5cfRPtWOitM3m7Mq26r7LpobMM+oOLUm4p0KKNIthWcmK9tYwXWSuGGfUQ+Y8gt7E0G06ZGbCPHOrxJ8lYQqXsif04piONPA/c9Hq43O99KPNGShONCS9oPFdOLRT3U= ostree-image-test"
+home = "/home/admin/"
+groups = ["wheel"]
+EOF
+
+greenprint "📄 Which blueprint are we using"
+cat "$BLUEPRINT_FILE"
+
+# Prepare the blueprint for the compose.
+greenprint "📋 Preparing blueprint"
+sudo composer-cli blueprints push "$BLUEPRINT_FILE"
+sudo composer-cli blueprints depsolve container
+
+# Build container image.
+build_image container "${CONTAINER_IMAGE_TYPE}" "$PARENT_REF"
+
+# Download the image
+greenprint "📥 Downloading the image"
+sudo composer-cli compose image "${COMPOSE_ID}" > /dev/null
+
+# Clear container running env
+greenprint "🧹 Clearing container running env"
+# Remove any status containers if exist
+sudo podman ps -a -q --format "{{.ID}}" | sudo xargs --no-run-if-empty podman rm -f
+# Remove all images
+sudo podman rmi -f -a
+
+# Prepare rhel-edge container network
+greenprint "Prepare container network"
+sudo podman network inspect edge >/dev/null 2>&1 || sudo podman network create --driver=bridge --subnet=192.168.200.0/24 --gateway=192.168.200.254 edge
+
+# Deal with stage repo image
+greenprint "🗜 Starting container"
+IMAGE_FILENAME="${COMPOSE_ID}-${CONTAINER_FILENAME}"
+sudo podman pull "oci-archive:${IMAGE_FILENAME}"
+sudo podman images
+greenprint "🛰 Running edge stage repo"
+# Get image id to run image
+EDGE_IMAGE_ID=$(sudo podman images --filter "dangling=true" --format "{{.ID}}")
+sudo podman run -d --name rhel-edge --network edge --ip "$STAGE_REPO_ADDRESS" "$EDGE_IMAGE_ID"
+# Clear image file
+sudo rm -f "$IMAGE_FILENAME"
+
+# Wait for container to be running
+until [ "$(sudo podman inspect -f '{{.State.Running}}' rhel-edge)" == "true" ]; do
+    sleep 1;
+done;
+
+# Clean compose and blueprints.
+greenprint "🧹 Clean up compose"
+sudo composer-cli compose delete "${COMPOSE_ID}" > /dev/null
+sudo composer-cli blueprints delete container > /dev/null
 
 # Sync installer ostree content
 greenprint "Sync rhel8 ostree repo"
-sudo ostree --repo="$PROD_REPO" pull --mirror edge-stage-8 "$PARENT_REF"
+sudo ostree --repo="$PROD_REPO" pull --mirror edge-stage "$PARENT_REF"
 
 # Write kickstart file for ostree image installation.
 greenprint "📑 Generate kickstart file"
@@ -351,7 +436,7 @@ sudo composer-cli blueprints push "$BLUEPRINT_FILE"
 sudo composer-cli blueprints depsolve upgrade
 
 # Build installer image.
-build_image upgrade "${CONTAINER_IMAGE_TYPE}"
+build_image upgrade "${CONTAINER_IMAGE_TYPE}" "$OSTREE_REF" "$PROD_REPO_URL" "$PARENT_REF"
 
 # Download the image
 greenprint "📥 Downloading the upgrade image"
@@ -363,10 +448,6 @@ greenprint "🧹 Clearing container running env"
 sudo podman ps -a -q --format "{{.ID}}" | sudo xargs --no-run-if-empty podman rm -f
 # Remove all images
 sudo podman rmi -f -a
-
-# Prepare rhel-edge container network
-greenprint "Prepare container network"
-sudo podman network inspect edge >/dev/null 2>&1 || sudo podman network create --driver=bridge --subnet=192.168.200.0/24 --gateway=192.168.200.254 edge
 
 # Deal with stage repo image
 greenprint "🗜 Starting container"
