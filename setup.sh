15ac8570f1795020443f67518789ab871f1625a7
Xiaofeng Wang <henrywangxf@me.com>
2022-08-20 19:44:25 +0800

    Add workaround for BZ#211621

--- setup.sh
+++ setup.sh
@@ -31,6 +31,11 @@ echo -e "\033[0m"
 # Get OS data.
 source /etc/os-release
 
+# Colorful output.
+function greenprint {
+    echo -e "\033[1;32m${1}\033[0m"
+}
+
 # set locale to en_US.UTF-8
 sudo dnf install -y glibc-langpack-en
 sudo localectl set-locale LANG=en_US.UTF-8
@@ -41,6 +46,17 @@ curl https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-
 sudo dnf install -y --nogpgcheck ansible-core
 # To support stdout_callback = yaml
 sudo ansible-galaxy collection install community.general
+
+# Install required packages
+greenprint "Install required packages"
+sudo dnf install -y --nogpgcheck httpd osbuild osbuild-composer composer-cli podman skopeo wget firewalld lorax xorriso curl jq expect qemu-img qemu-kvm libvirt-client libvirt-daemon-kvm virt-install
+if [[ $ID == "centos" && $VERSION_ID == "8" ]]; then
+    # Workaround for https://bugzilla.redhat.com/show_bug.cgi?id=2065292
+    # Remove when podman-4.0.2-2.el8 is in Centos 8 repositories
+    greenprint "Updating libseccomp on Centos 8"
+    sudo dnf upgrade -y libseccomp
+fi
+sudo rpm -qa | grep -i osbuild
 
 # Customize repository
 sudo mkdir -p /etc/osbuild-composer/repositories
@@ -54,11 +70,15 @@ case "${ID}-${VERSION_ID}" in
         sudo cp files/rhel-8-6-0-sha512.json /etc/osbuild-composer/repositories/rhel-86.json
         sudo cp files/rhel-9-0-0.json /etc/osbuild-composer/repositories/rhel-90.json;;
     "rhel-9.1")
+        # Wordaround bug https://bugzilla.redhat.com/show_bug.cgi?id=2116221
+        sudo dnf install -y http://download.eng.bos.redhat.com/brewroot/vol/rhel-9/packages/aardvark-dns/1.1.0/2.el9/x86_64/aardvark-dns-1.1.0-2.el9.x86_64.rpm
         sudo cp files/rhel-8-7-0-sha512.json /etc/osbuild-composer/repositories/rhel-87.json
         sudo cp files/rhel-9-1-0.json /etc/osbuild-composer/repositories/rhel-91.json;;
     "centos-8")
         sudo cp files/centos-stream-8.json /etc/osbuild-composer/repositories/centos-8.json;;
     "centos-9")
+        # Wordaround bug https://bugzilla.redhat.com/show_bug.cgi?id=2116221
+        sudo dnf install -y https://kojihub.stream.centos.org/kojifiles/packages/aardvark-dns/1.1.0/2.el9/x86_64/aardvark-dns-1.1.0-2.el9.x86_64.rpm
         sudo cp files/centos-stream-8.json /etc/osbuild-composer/repositories/centos-8.json
         sudo cp files/centos-stream-9.json /etc/osbuild-composer/repositories/centos-9.json;;
     "fedora-36")
@@ -72,11 +92,6 @@ case "${ID}-${VERSION_ID}" in
         exit 1;;
 esac
 
-# Colorful output.
-function greenprint {
-    echo -e "\033[1;32m${1}\033[0m"
-}
-
 # Check ostree_key permissions
 KEY_PERMISSION_PRE=$(stat -L -c "%a %G %U" key/ostree_key | grep -oP '\d+' | head -n 1)
 echo -e "${KEY_PERMISSION_PRE}"
@@ -84,17 +99,6 @@ if [[ "${KEY_PERMISSION_PRE}" != "600" ]]; then
    greenprint "💡 File permissions too open...Changing to 600"
    chmod 600 ./key/ostree_key
 fi
-
-# Install required packages
-greenprint "Install required packages"
-sudo dnf install -y --nogpgcheck httpd osbuild osbuild-composer composer-cli podman skopeo wget firewalld lorax xorriso curl jq expect qemu-img qemu-kvm libvirt-client libvirt-daemon-kvm virt-install
-if [[ $ID == "centos" && $VERSION_ID == "8" ]]; then
-    # Workaround for https://bugzilla.redhat.com/show_bug.cgi?id=2065292
-    # Remove when podman-4.0.2-2.el8 is in Centos 8 repositories
-    greenprint "Updating libseccomp on Centos 8"
-    sudo dnf upgrade -y libseccomp
-fi
-sudo rpm -qa | grep -i osbuild
 
 # Start httpd server as prod ostree repo
 greenprint "Start httpd service"
