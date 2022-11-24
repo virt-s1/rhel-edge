efb465be231f407c3667e2bad086f4b91e61e820
Xiaofeng Wang <henrywangxf@me.com>
2022-11-24 18:10:30 +0800

    Enable raw image test on Fedora 37 and 38

--- ostree-raw-image.sh
+++ ostree-raw-image.sh
@@ -26,6 +26,9 @@ RAW_TYPE=edge-raw-image
 RAW_FILENAME=image.raw.xz
 # Workaround BZ#2108646
 BOOT_ARGS="uefi"
+# RHEL and CS default ostree os_name is redhat
+# But Fedora uses fedora-iot
+ANSIBLE_OS_NAME="redhat"
 
 # Set up temporary files.
 TEMPDIR=$(mktemp -d)
@@ -47,42 +50,70 @@ case "${ID}-${VERSION_ID}" in
         OSTREE_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8-unknown"
         ADD_SSSD="false"
+        USER_IN_RAW="false"
         ;;
     "rhel-8.7")
         OSTREE_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8.7"
         ADD_SSSD="true"
+        USER_IN_RAW="false"
         ;;
     "rhel-8.8")
         OSTREE_REF="rhel/8/${ARCH}/edge"
         OS_VARIANT="rhel8-unknown"
         ADD_SSSD="true"
+        USER_IN_RAW="true"
         ;;
     "rhel-9.0")
         OSTREE_REF="rhel/9/${ARCH}/edge"
         OS_VARIANT="rhel9.0"
         ADD_SSSD="false"
+        USER_IN_RAW="false"
         ;;
     "rhel-9.1")
         OSTREE_REF="rhel/9/${ARCH}/edge"
         OS_VARIANT="rhel9.1"
         ADD_SSSD="true"
+        USER_IN_RAW="false"
         ;;
     "rhel-9.2")
         OSTREE_REF="rhel/9/${ARCH}/edge"
         OS_VARIANT="rhel9-unknown"
         ADD_SSSD="true"
+        USER_IN_RAW="true"
         ;;
     "centos-8")
         OSTREE_REF="centos/8/${ARCH}/edge"
         OS_VARIANT="centos-stream8"
         ADD_SSSD="true"
+        USER_IN_RAW="true"
         ;;
     "centos-9")
         OSTREE_REF="centos/9/${ARCH}/edge"
         OS_VARIANT="centos-stream9"
         ADD_SSSD="true"
         BOOT_ARGS="uefi,firmware.feature0.name=secure-boot,firmware.feature0.enabled=no"
+        USER_IN_RAW="true"
+        ;;
+    "fedora-37")
+        CONTAINER_TYPE=fedora-iot-container
+        RAW_TYPE=iot-raw-image
+        OSTREE_REF="fedora/37/${ARCH}/iot"
+        OS_VARIANT="fedora-unknown"
+        ADD_SSSD="false"
+        ANSIBLE_OS_NAME="fedora-iot"
+        USER_IN_RAW="true"
+        REF_PREFIX="fedora-iot"
+        ;;
+    "fedora-38")
+        CONTAINER_TYPE=fedora-iot-container
+        RAW_TYPE=iot-raw-image
+        OSTREE_REF="fedora/38/${ARCH}/iot"
+        OS_VARIANT="fedora-unknown"
+        ADD_SSSD="false"
+        ANSIBLE_OS_NAME="fedora-iot"
+        USER_IN_RAW="true"
+        REF_PREFIX="fedora-iot"
         ;;
     *)
         echo "unsupported distro: ${ID}-${VERSION_ID}"
@@ -92,6 +123,27 @@ esac
 # Colorful output.
 function greenprint {
     echo -e "\033[1;32m${1}\033[0m"
+}
+
+# Compare rpm package version
+function nvrGreaterOrEqual {
+    local rpm_name=$1
+    local min_version=$2
+
+    set +e
+
+    rpm_version=$(rpm -q --qf "%{version}" "${rpm_name}")
+    rpmdev-vercmp "${rpm_version}" "${min_version}" 1>&2
+    if [ "$?" != "12" ]; then
+        # 0 - rpm_version == min_version
+        # 11 - rpm_version > min_version
+        # 12 - rpm_version < min_version
+        set -e
+        return
+    fi
+
+    set -e
+    false
 }
 
 # Get the compose log.
@@ -140,14 +192,23 @@ build_image() {
     else
         sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
     fi
-    COMPOSE_ID=$(jq -r '.body.build_id' "$COMPOSE_START")
+
+    if nvrGreaterOrEqual "weldr-client" "35.6"; then
+        COMPOSE_ID=$(jq -r '.[0].body.build_id' "$COMPOSE_START")
+    else
+        COMPOSE_ID=$(jq -r '.body.build_id' "$COMPOSE_START")
+    fi
 
     # Wait for the compose to finish.
     greenprint "⏱ Waiting for compose to finish: ${COMPOSE_ID}"
     while true; do
         sudo composer-cli --json compose info "${COMPOSE_ID}" | tee "$COMPOSE_INFO" > /dev/null
 
-        COMPOSE_STATUS=$(jq -r '.body.queue_status' "$COMPOSE_INFO")
+        if nvrGreaterOrEqual "weldr-client" "35.6"; then
+            COMPOSE_STATUS=$(jq -r '.[0].body.queue_status' "$COMPOSE_INFO")
+        else
+            COMPOSE_STATUS=$(jq -r '.body.queue_status' "$COMPOSE_INFO")
+        fi
 
         # Is the compose finished?
         if [[ $COMPOSE_STATUS != RUNNING ]] && [[ $COMPOSE_STATUS != WAITING ]]; then
@@ -256,18 +317,15 @@ groups = []
 [[packages]]
 name = "python3"
 version = "*"
+EOF
 
+# Fedora does not have kernel-rt
+if [[ "$ID" != "fedora" ]]; then
+    tee -a "$BLUEPRINT_FILE" >> /dev/null << EOF
 [customizations.kernel]
 name = "kernel-rt"
-
-[[customizations.user]]
-name = "admin"
-description = "Administrator account"
-password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHBKnB2FBXGQ/OkwZQfW/76ktHd0NX5nls2LPxPuUdl."
-key = "${SSH_KEY_PUB}"
-home = "/home/admin/"
-groups = ["wheel"]
 EOF
+fi
 
 # For BZ#2088459, RHEL 8.6 and 9.0 will not have new release which fix this issue
 # RHEL 8.6 and 9.0 will not include sssd package in blueprint
@@ -276,6 +334,19 @@ if [[ "${ADD_SSSD}" == "true" ]]; then
 [[packages]]
 name = "sssd"
 version = "*"
+EOF
+fi
+
+# User in raw image blueprint is not for RHEL 9.1, 8.7 and before
+if [[ "$USER_IN_RAW" == "false" ]]; then
+    tee -a "$BLUEPRINT_FILE" > /dev/null << EOF
+[[customizations.user]]
+name = "admin"
+description = "Administrator account"
+password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHBKnB2FBXGQ/OkwZQfW/76ktHd0NX5nls2LPxPuUdl."
+key = "${SSH_KEY_PUB}"
+home = "/home/admin/"
+groups = ["wheel"]
 EOF
 fi
 
@@ -352,6 +423,19 @@ modules = []
 groups = []
 EOF
 
+# User in raw image blueprint is not for RHEL 9.1, 8.7 and before
+if [[ "$USER_IN_RAW" == "true" ]]; then
+    tee -a "$BLUEPRINT_FILE" > /dev/null << EOF
+[[customizations.user]]
+name = "admin"
+description = "Administrator account"
+password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHBKnB2FBXGQ/OkwZQfW/76ktHd0NX5nls2LPxPuUdl."
+key = "${SSH_KEY_PUB}"
+home = "/home/admin/"
+groups = ["wheel"]
+EOF
+fi
+
 greenprint "📄 raw image blueprint"
 cat "$BLUEPRINT_FILE"
 
@@ -381,9 +465,13 @@ sudo composer-cli compose delete "${COMPOSE_ID}" > /dev/null
 sudo composer-cli blueprints delete raw > /dev/null
 
 # Prepare qcow2 file for tests
-sudo cp "${IMAGE_KEY}.qcow2" /var/lib/libvirt/images/"${IMAGE_KEY}-bios.qcow2"
+# Fedora raw image does not support BIOS
+# https://github.com/osbuild/osbuild-composer/pull/3038/commits/fa1417e4ba031af343314ff6959a974ace117b19
+if [[ "$ID" != "fedora" ]]; then
+    sudo cp "${IMAGE_KEY}.qcow2" /var/lib/libvirt/images/"${IMAGE_KEY}-bios.qcow2"
+    LIBVIRT_IMAGE_PATH_BIOS=/var/lib/libvirt/images/"${IMAGE_KEY}-bios.qcow2"
+fi
 sudo cp "${IMAGE_KEY}.qcow2" /var/lib/libvirt/images/"${IMAGE_KEY}-uefi.qcow2"
-LIBVIRT_IMAGE_PATH_BIOS=/var/lib/libvirt/images/"${IMAGE_KEY}-bios.qcow2"
 LIBVIRT_IMAGE_PATH_UEFI=/var/lib/libvirt/images/"${IMAGE_KEY}-uefi.qcow2"
 
 # Ensure SELinux is happy with our new images.
@@ -392,48 +480,52 @@ sudo restorecon -Rv /var/lib/libvirt/images/
 
 # Remove qcow2 file
 sudo rm -f "${IMAGE_KEY}.qcow2"
-##################################################################
-##
-## Install and test edge vm with edge-raw-image (BIOS)
-##
-##################################################################
-greenprint "💿 Installing raw image on BIOS VM"
-sudo virt-install  --name="${IMAGE_KEY}-bios"\
-                   --disk path="${LIBVIRT_IMAGE_PATH_BIOS}",format=qcow2 \
-                   --ram 3072 \
-                   --vcpus 2 \
-                   --network network=integration,mac=34:49:22:B0:83:30 \
-                   --import \
-                   --os-type linux \
-                   --os-variant ${OS_VARIANT} \
-                   --nographics \
-                   --noautoconsole \
-                   --wait=-1 \
-                   --noreboot
 
-# Start VM.
-greenprint "💻 Start BIOS VM"
-sudo virsh start "${IMAGE_KEY}-bios"
+# Fedora raw image does not support BIOS
+# https://github.com/osbuild/osbuild-composer/pull/3038/commits/fa1417e4ba031af343314ff6959a974ace117b19
+if [[ "$ID" != "fedora" ]]; then
+    ##################################################################
+    ##
+    ## Install and test edge vm with edge-raw-image (BIOS)
+    ##
+    ##################################################################
+    greenprint "💿 Installing raw image on BIOS VM"
+    sudo virt-install  --name="${IMAGE_KEY}-bios"\
+                    --disk path="${LIBVIRT_IMAGE_PATH_BIOS}",format=qcow2 \
+                    --ram 3072 \
+                    --vcpus 2 \
+                    --network network=integration,mac=34:49:22:B0:83:30 \
+                    --import \
+                    --os-type linux \
+                    --os-variant ${OS_VARIANT} \
+                    --nographics \
+                    --noautoconsole \
+                    --wait=-1 \
+                    --noreboot
 
-# Check for ssh ready to go.
-greenprint "🛃 Checking for SSH is ready to go"
-for LOOP_COUNTER in $(seq 0 30); do
-    RESULTS="$(wait_for_ssh_up $BIOS_GUEST_ADDRESS)"
-    if [[ $RESULTS == 1 ]]; then
-        echo "SSH is ready now! 🥳"
-        break
-    fi
-    sleep 10
-done
+    # Start VM.
+    greenprint "💻 Start BIOS VM"
+    sudo virsh start "${IMAGE_KEY}-bios"
 
-# Check image installation result
-check_result
+    # Check for ssh ready to go.
+    greenprint "🛃 Checking for SSH is ready to go"
+    for LOOP_COUNTER in $(seq 0 30); do
+        RESULTS="$(wait_for_ssh_up $BIOS_GUEST_ADDRESS)"
+        if [[ $RESULTS == 1 ]]; then
+            echo "SSH is ready now! 🥳"
+            break
+        fi
+        sleep 10
+    done
 
-greenprint "🕹 Get ostree install commit value"
-INSTALL_HASH=$(curl "${PROD_REPO_URL}/refs/heads/${OSTREE_REF}")
+    # Check image installation result
+    check_result
 
-# Add instance IP address into /etc/ansible/hosts
-tee "${TEMPDIR}"/inventory > /dev/null << EOF
+    greenprint "🕹 Get ostree install commit value"
+    INSTALL_HASH=$(curl "${PROD_REPO_URL}/refs/heads/${OSTREE_REF}")
+
+    # Add instance IP address into /etc/ansible/hosts
+    tee "${TEMPDIR}"/inventory > /dev/null << EOF
 [ostree_guest]
 ${BIOS_GUEST_ADDRESS}
 
@@ -447,17 +539,20 @@ ansible_become_method=sudo
 ansible_become_pass=${EDGE_USER_PASSWORD}
 EOF
 
-# Test IoT/Edge OS
-podman run -v "$(pwd)":/work:z -v "${TEMPDIR}":/tmp:z --rm quay.io/rhel-edge/ansible-runner:latest ansible-playbook -v -i /tmp/inventory -e os_name=redhat -e ostree_commit="${INSTALL_HASH}" -e ostree_ref="${REF_PREFIX}:${OSTREE_REF}" check-ostree.yaml || RESULTS=0
-check_result
+    # Test IoT/Edge OS
+    podman run -v "$(pwd)":/work:z -v "${TEMPDIR}":/tmp:z --rm quay.io/rhel-edge/ansible-runner:latest ansible-playbook -v -i /tmp/inventory -e os_name="${ANSIBLE_OS_NAME}" -e ostree_commit="${INSTALL_HASH}" -e ostree_ref="${REF_PREFIX}:${OSTREE_REF}" check-ostree.yaml || RESULTS=0
+    check_result
 
-# Clean up BIOS VM
-greenprint "🧹 Clean up BIOS VM"
-if [[ $(sudo virsh domstate "${IMAGE_KEY}-bios") == "running" ]]; then
-    sudo virsh destroy "${IMAGE_KEY}-bios"
+    # Clean up BIOS VM
+    greenprint "🧹 Clean up BIOS VM"
+    if [[ $(sudo virsh domstate "${IMAGE_KEY}-bios") == "running" ]]; then
+        sudo virsh destroy "${IMAGE_KEY}-bios"
+    fi
+    sudo virsh undefine "${IMAGE_KEY}-bios"
+    sudo virsh vol-delete --pool images "${IMAGE_KEY}-bios.qcow2"
+else
+    greenprint "Skipping BIOS boot for Fedora IoT (not supported)"
 fi
-sudo virsh undefine "${IMAGE_KEY}-bios"
-sudo virsh vol-delete --pool images "${IMAGE_KEY}-bios.qcow2"
 
 ##################################################################
 ##
@@ -476,6 +571,7 @@ sudo virt-install  --name="${IMAGE_KEY}-uefi"\
                    --nographics \
                    --noautoconsole \
                    --wait=-1 \
+                   --import \
                    --noreboot
 
 # Start VM.
@@ -515,7 +611,7 @@ ansible_become_pass=${EDGE_USER_PASSWORD}
 EOF
 
 # Test IoT/Edge OS
-podman run -v "$(pwd)":/work:z -v "${TEMPDIR}":/tmp:z --rm quay.io/rhel-edge/ansible-runner:latest ansible-playbook -v -i /tmp/inventory -e os_name=redhat -e ostree_commit="${INSTALL_HASH}" -e ostree_ref="${REF_PREFIX}:${OSTREE_REF}" check-ostree.yaml || RESULTS=0
+podman run -v "$(pwd)":/work:z -v "${TEMPDIR}":/tmp:z --rm quay.io/rhel-edge/ansible-runner:latest ansible-playbook -v -i /tmp/inventory -e os_name="${ANSIBLE_OS_NAME}" -e ostree_commit="${INSTALL_HASH}" -e ostree_ref="${REF_PREFIX}:${OSTREE_REF}" check-ostree.yaml || RESULTS=0
 check_result
 
 ##################################################################
@@ -541,9 +637,6 @@ version = "*"
 name = "wget"
 version = "*"
 
-[customizations.kernel]
-name = "kernel-rt"
-
 [[customizations.user]]
 name = "admin"
 description = "Administrator account"
@@ -551,6 +644,14 @@ password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHB
 home = "/home/admin/"
 groups = ["wheel"]
 EOF
+
+# Fedora does not have kernel-rt
+if [[ "$ID" != "fedora" ]]; then
+    tee -a "$BLUEPRINT_FILE" >> /dev/null << EOF
+[customizations.kernel]
+name = "kernel-rt"
+EOF
+fi
 
 # For BZ#2088459, RHEL 8.6 and 9.0 will not have new release which fix this issue
 # RHEL 8.6 and 9.0 will not include sssd package in blueprint
@@ -617,6 +718,14 @@ greenprint "🧽 Clean up upgrade blueprint and compose"
 sudo composer-cli compose delete "${COMPOSE_ID}" > /dev/null
 sudo composer-cli blueprints delete upgrade > /dev/null
 
+if [[ "$ID" == "fedora" ]]; then
+    # The Fedora IoT Raw image sets the fedora-iot remote URL to https://ostree.fedoraproject.org/iot
+    # Replacing with our own local repo
+    greenprint "Replacing default remote"
+    sudo ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" admin@${UEFI_GUEST_ADDRESS} "echo ${EDGE_USER_PASSWORD} |sudo -S ostree remote delete ${ANSIBLE_OS_NAME}"
+    sudo ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" admin@${UEFI_GUEST_ADDRESS} "echo ${EDGE_USER_PASSWORD} |sudo -S ostree remote add --no-gpg-verify ${ANSIBLE_OS_NAME} ${PROD_REPO_URL}"
+fi
+
 # Upgrade image/commit.
 greenprint "🗳 Upgrade ostree image/commit"
 sudo ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" admin@${UEFI_GUEST_ADDRESS} "echo ${EDGE_USER_PASSWORD} |sudo -S rpm-ostree upgrade"
@@ -656,7 +765,7 @@ ansible_become_pass=${EDGE_USER_PASSWORD}
 EOF
 
 # Test IoT/Edge OS
-podman run -v "$(pwd)":/work:z -v "${TEMPDIR}":/tmp:z --rm quay.io/rhel-edge/ansible-runner:latest ansible-playbook -v -i /tmp/inventory -e os_name=redhat -e ostree_commit="${UPGRADE_HASH}" -e ostree_ref="${REF_PREFIX}:${OSTREE_REF}" check-ostree.yaml || RESULTS=0
+podman run -v "$(pwd)":/work:z -v "${TEMPDIR}":/tmp:z --rm quay.io/rhel-edge/ansible-runner:latest ansible-playbook -v -i /tmp/inventory -e os_name="${ANSIBLE_OS_NAME}" -e ostree_commit="${UPGRADE_HASH}" -e ostree_ref="${REF_PREFIX}:${OSTREE_REF}" check-ostree.yaml || RESULTS=0
 check_result
 
 # Final success clean up
