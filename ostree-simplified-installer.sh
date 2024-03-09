13521aacf3b4871075d7f8518602d68c51a9669b
Xiaofeng Wang <henrywangxf@me.com>
2024-03-09 08:36:53 +0800

    test: add FDO postgres db test
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- ostree-simplified-installer.sh
+++ ostree-simplified-installer.sh
@@ -4,57 +4,14 @@ set -euox pipefail
 # Provision the software under test.
 ./setup.sh
 
+# Colorful output.
+function greenprint {
+    echo -e "\033[1;32m${1}\033[0m"
+}
+
 # Get OS data.
 source /etc/os-release
 ARCH=$(uname -m)
-
-# Install FDO packages (This cannot be done in the setup.sh because FDO packages are not available on fedora)
-sudo dnf install -y \
-    fdo-admin-cli \
-    fdo-rendezvous-server \
-    fdo-owner-onboarding-server \
-    fdo-owner-cli \
-    fdo-manufacturing-server \
-    python3-pip
-
-# Generate key and cert used by FDO
-sudo mkdir -p /etc/fdo/keys
-for obj in diun manufacturer device-ca owner; do
-    sudo fdo-admin-tool generate-key-and-cert --destination-dir /etc/fdo/keys "$obj"
-done
-
-# Copy configuration files
-sudo mkdir -p \
-    /etc/fdo/manufacturing-server.conf.d/ \
-    /etc/fdo/owner-onboarding-server.conf.d/ \
-    /etc/fdo/rendezvous-server.conf.d/ \
-    /etc/fdo/serviceinfo-api-server.conf.d/
-
-sudo cp files/fdo/manufacturing-server.yml /etc/fdo/manufacturing-server.conf.d/
-sudo cp files/fdo/owner-onboarding-server.yml /etc/fdo/owner-onboarding-server.conf.d/
-sudo cp files/fdo/rendezvous-server.yml /etc/fdo/rendezvous-server.conf.d/
-sudo cp files/fdo/serviceinfo-api-server.yml /etc/fdo/serviceinfo-api-server.conf.d/
-
-# Install yq to modify service api server config yaml file
-# Workaround - https://issues.redhat.com/browse/RHEL-21528
-if [[ "${ID}-${VERSION_ID}" == "rhel-8.8" ]] || [[ "${ID}-${VERSION_ID}" == "rhel-8.6" ]]; then
-    sudo yum update -y platform-python
-fi
-# end workaround
-sudo pip3 install yq
-# Prepare service api server config file
-sudo /usr/local/bin/yq -iy '.service_info.diskencryption_clevis |= [{disk_label: "/dev/vda4", reencrypt: true, binding: {pin: "tpm2", config: "{}"}}]' /etc/fdo/serviceinfo-api-server.conf.d/serviceinfo-api-server.yml
-# Fedora iot-simplified-installer uses /dev/vda3, https://github.com/osbuild/osbuild-composer/issues/3527
-if [[ "${ID}" == "fedora" ]]; then
-    echo "Change vda4 to vda3 for fedora in serviceinfo config file"
-    sudo sed -i 's/vda4/vda3/' /etc/fdo/serviceinfo-api-server.conf.d/serviceinfo-api-server.yml
-fi
-# Start FDO services
-sudo systemctl start \
-    fdo-owner-onboarding-server.service \
-    fdo-rendezvous-server.service \
-    fdo-manufacturing-server.service \
-    fdo-serviceinfo-api-server.service
 
 # Set up variables.
 TEST_UUID=$(uuidgen)
@@ -68,8 +25,6 @@ PROD_REPO=/var/www/html/repo
 STAGE_REPO_ADDRESS=192.168.200.1
 STAGE_REPO_URL="http://${STAGE_REPO_ADDRESS}:8080/repo/"
 FDO_SERVER_ADDRESS=192.168.100.1
-DIUN_PUB_KEY_HASH=sha256:$(openssl x509 -fingerprint -sha256 -noout -in /etc/fdo/keys/diun_cert.pem | cut -d"=" -f2 | sed 's/://g')
-DIUN_PUB_KEY_ROOT_CERTS=$(cat /etc/fdo/keys/diun_cert.pem)
 CONTAINER_TYPE=edge-container
 CONTAINER_FILENAME=container.tar
 INSTALLER_TYPE=edge-simplified-installer
@@ -90,7 +45,6 @@ SSH_KEY=key/ostree_key
 SSH_KEY_PUB=$(cat "${SSH_KEY}".pub)
 EDGE_USER_PASSWORD=foobar
 ANSIBLE_USER="admin"
-FDO_USER_ONBOARDING="false"
 USER_IN_BLUEPRINT="false"
 BLUEPRINT_USER="admin"
 
@@ -101,6 +55,10 @@ SYSROOT_RO="false"
 # No FDO and Ignition in simplified installer is only supported started from 8.8 and 9.2
 NO_FDO="false"
 OS_NAME="redhat"
+
+# FDO variables
+FDO_USER_ONBOARDING="false"
+FDO_DB="false"
 
 # Prepare osbuild-composer repository file
 sudo mkdir -p /etc/osbuild-composer/repositories
@@ -188,7 +146,8 @@ case "${ID}-${VERSION_ID}" in
         FDO_USER_ONBOARDING="true"
         USER_IN_BLUEPRINT="true"
         BLUEPRINT_USER="simple"
-        NO_FDO="true"
+        NO_FDO="false"
+        FDO_DB="true"
         ;;
     "centos-8")
         OSTREE_REF="centos/8/${ARCH}/edge"
@@ -217,6 +176,7 @@ case "${ID}-${VERSION_ID}" in
         USER_IN_BLUEPRINT="true"
         BLUEPRINT_USER="simple"
         NO_FDO="true"
+        FDO_DB="true"
         ;;
     "fedora-"*)
         OSTREE_REF="fedora/${VERSION_ID}/${ARCH}/iot"
@@ -239,6 +199,131 @@ case "${ID}-${VERSION_ID}" in
         exit 1;;
 esac
 
+# Clear container running env
+greenprint "🧹 Clearing container running env"
+# Remove any status containers if exist
+sudo podman ps -a -q --format "{{.ID}}" | sudo xargs --no-run-if-empty podman rm -f
+# Remove all images
+sudo podman rmi -f -a
+
+# Prepare stage repo network
+greenprint "🔧 Prepare stage repo network"
+sudo podman network inspect edge >/dev/null 2>&1 || sudo podman network create --driver=bridge --subnet=192.168.200.0/24 --gateway=192.168.200.254 edge
+
+# Install FDO packages (This cannot be done in the setup.sh because FDO packages are not available on fedora)
+sudo dnf install -y \
+    fdo-admin-cli \
+    fdo-rendezvous-server \
+    fdo-owner-onboarding-server \
+    fdo-owner-cli \
+    fdo-manufacturing-server \
+    python3-pip
+
+# Generate key and cert used by FDO
+sudo mkdir -p /etc/fdo/keys
+for obj in diun manufacturer device-ca owner; do
+    sudo fdo-admin-tool generate-key-and-cert --destination-dir /etc/fdo/keys "$obj"
+done
+
+# Copy configuration files
+sudo mkdir -p \
+    /etc/fdo/manufacturing-server.conf.d/ \
+    /etc/fdo/owner-onboarding-server.conf.d/ \
+    /etc/fdo/rendezvous-server.conf.d/ \
+    /etc/fdo/serviceinfo-api-server.conf.d/
+
+sudo cp files/fdo/manufacturing-server.yml /etc/fdo/manufacturing-server.conf.d/
+sudo cp files/fdo/owner-onboarding-server.yml /etc/fdo/owner-onboarding-server.conf.d/
+sudo cp files/fdo/rendezvous-server.yml /etc/fdo/rendezvous-server.conf.d/
+sudo cp files/fdo/serviceinfo-api-server.yml /etc/fdo/serviceinfo-api-server.conf.d/
+
+# Install yq to modify service api server config yaml file
+# Workaround - https://issues.redhat.com/browse/RHEL-21528
+if [[ "${ID}-${VERSION_ID}" == "rhel-8.8" ]] || [[ "${ID}-${VERSION_ID}" == "rhel-8.6" ]]; then
+    sudo yum update -y platform-python
+fi
+# end workaround
+sudo pip3 install yq
+# Prepare service api server config file
+sudo /usr/local/bin/yq -iy '.service_info.diskencryption_clevis |= [{disk_label: "/dev/vda4", reencrypt: true, binding: {pin: "tpm2", config: "{}"}}]' /etc/fdo/serviceinfo-api-server.conf.d/serviceinfo-api-server.yml
+# Fedora iot-simplified-installer uses /dev/vda3, https://github.com/osbuild/osbuild-composer/issues/3527
+if [[ "${ID}" == "fedora" ]]; then
+    echo "Change vda4 to vda3 for fedora in serviceinfo config file"
+    sudo sed -i 's/vda4/vda3/' /etc/fdo/serviceinfo-api-server.conf.d/serviceinfo-api-server.yml
+fi
+
+if [[ "$FDO_DB" == "true" ]]; then
+    POSTGRES_USERNAME=postgres
+    POSTGRES_PASSWORD=foobar
+    POSTGRES_DB=postgres
+    POSTGRES_IP=192.168.200.2
+
+    # Disable selinux as a workaround
+    sudo setenforce 0
+    getenforce
+
+    # Set servers store driver to postgres
+    greenprint "🔧 Set servers store driver to postgres"
+    # Configure manufacturing server db
+    sudo /usr/local/bin/yq -yi 'del(.ownership_voucher_store_driver.Directory)' /etc/fdo/manufacturing-server.conf.d/manufacturing-server.yml
+    sudo /usr/local/bin/yq -yi '.ownership_voucher_store_driver += {"Postgres": "Manufacturer"}' /etc/fdo/manufacturing-server.conf.d/manufacturing-server.yml
+    # Configure owner onboarding server db
+    sudo /usr/local/bin/yq -yi 'del(.ownership_voucher_store_driver.Directory)' /etc/fdo/owner-onboarding-server.conf.d/owner-onboarding-server.yml
+    sudo /usr/local/bin/yq -yi '.ownership_voucher_store_driver += {"Postgres": "Owner"}' /etc/fdo/owner-onboarding-server.conf.d/owner-onboarding-server.yml
+    # Configure rendezvous server db
+    sudo /usr/local/bin/yq -yi 'del(.storage_driver.Directory)' /etc/fdo/rendezvous-server.conf.d/rendezvous-server.yml
+    sudo /usr/local/bin/yq -yi '.storage_driver += {"Postgres": "Rendezvous"}' /etc/fdo/rendezvous-server.conf.d/rendezvous-server.yml
+
+    # Configure environment variables for Postgres connection
+    greenprint "🔧 Configure environment variables for Postgres connection"
+    sudo sed -i \
+        "/Environment=LOG_LEVEL=info/a Environment=POSTGRES_MANUFACTURER_DATABASE_URL=postgresql:\/\/${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_IP}\/${POSTGRES_DB}" \
+        /usr/lib/systemd/system/fdo-manufacturing-server.service
+    sudo sed -i \
+        "/Environment=LOG_LEVEL=info/a Environment=POSTGRES_OWNER_DATABASE_URL=postgresql:\/\/${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_IP}\/${POSTGRES_DB}" \
+        /usr/lib/systemd/system/fdo-owner-onboarding-server.service
+    sudo sed -i \
+        "/Environment=LOG_LEVEL=info/a Environment=POSTGRES_RENDEZVOUS_DATABASE_URL=postgresql:\/\/${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_IP}\/${POSTGRES_DB}" \
+        /usr/lib/systemd/system/fdo-rendezvous-server.service
+
+    # Prepare postgres db init sql script
+    greenprint "🔧 Prepare postgres db init sql script"
+    mkdir -p initdb
+    cp /usr/share/doc/fdo/migrations/migrations_manufacturing_server_postgres/up.sql initdb/manufacturing.sql
+    cp /usr/share/doc/fdo/migrations/migrations_owner_onboarding_server_postgres/up.sql initdb/owner-onboarding.sql
+    cp /usr/share/doc/fdo/migrations/migrations_rendezvous_server_postgres/up.sql initdb/rendezvous.sql
+
+    greenprint "🔧 Starting postgres"
+    sudo podman run -d \
+        --ip "$POSTGRES_IP" \
+        --name postgres \
+        --network edge \
+        -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+        -v "$PWD"/initdb/:/docker-entrypoint-initdb.d/:z \
+        "quay.io/xiaofwan/postgres"
+    until sudo podman exec postgres pg_isready
+    do
+        sleep 5
+    done
+fi
+
+# Start FDO services
+sudo systemctl start \
+    fdo-owner-onboarding-server.service \
+    fdo-rendezvous-server.service \
+    fdo-manufacturing-server.service \
+    fdo-serviceinfo-api-server.service
+
+# Sleep 10 seconds for service start
+sleep 10
+
+# Check all FDO service status
+sudo systemctl status \
+    fdo-owner-onboarding-server.service \
+    fdo-rendezvous-server.service \
+    fdo-manufacturing-server.service \
+    fdo-serviceinfo-api-server.service
+
 if [[ "$FDO_USER_ONBOARDING" == "true" ]]; then
     # FDO user does not have password, use ssh key and no sudo password instead
     sudo /usr/local/bin/yq -iy '.service_info.initial_user |= {username: "fdouser", sshkeys: ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzxo5dEcS+LDK/OFAfHo6740EyoDM8aYaCkBala0FnWfMMTOq7PQe04ahB0eFLS3IlQtK5bpgzxBdFGVqF6uT5z4hhaPjQec0G3+BD5Pxo6V+SxShKZo+ZNGU3HVrF9p2V7QH0YFQj5B8F6AicA3fYh2BVUFECTPuMpy5A52ufWu0r4xOFmbU7SIhRQRAQz2u4yjXqBsrpYptAvyzzoN4gjUhNnwOHSPsvFpWoBFkWmqn0ytgHg3Vv9DlHW+45P02QH1UFedXR2MqLnwRI30qqtaOkVS+9rE/dhnR+XPpHHG+hv2TgMDAuQ3IK7Ab5m/yCbN73cxFifH4LST0vVG3Jx45xn+GTeHHhfkAfBSCtya6191jixbqyovpRunCBKexI5cfRPtWOitM3m7Mq26r7LpobMM+oOLUm4p0KKNIthWcmK9tYwXWSuGGfUQ+Y8gt7E0G06ZGbCPHOrxJ8lYQqXsif04piONPA/c9Hq43O99KPNGShONCS9oPFdOLRT3U= ostree-image-test"]}' /etc/fdo/serviceinfo-api-server.conf.d/serviceinfo-api-server.yml
@@ -253,10 +338,73 @@ EOF
     sudo systemctl restart fdo-serviceinfo-api-server.service
 fi
 
-# Colorful output.
-function greenprint {
-    echo -e "\033[1;32m${1}\033[0m"
-}
+# Wait for fdo server to be running
+until [ "$(curl -X POST http://${FDO_SERVER_ADDRESS}:8080/ping)" == "pong" ]; do
+    sleep 1;
+done;
+
+until [ "$(curl -X POST http://${FDO_SERVER_ADDRESS}:8081/ping)" == "pong" ]; do
+    sleep 1;
+done;
+
+until [ "$(curl -X POST http://${FDO_SERVER_ADDRESS}:8082/ping)" == "pong" ]; do
+    sleep 1;
+done;
+
+DIUN_PUB_KEY_HASH=sha256:$(openssl x509 -fingerprint -sha256 -noout -in /etc/fdo/keys/diun_cert.pem | cut -d"=" -f2 | sed 's/://g')
+DIUN_PUB_KEY_ROOT_CERTS=$(cat /etc/fdo/keys/diun_cert.pem)
+
+# Check postgres db table
+if [[ "$FDO_DB" == "true" ]]; then
+    greenprint "🔧 Check container running status"
+    sudo podman ps -a
+
+    greenprint "🔧 Collecting container logs"
+    sudo podman logs postgres
+
+    greenprint "🔧 Check db tables"
+    sudo podman exec \
+        postgres \
+        psql \
+        --username="${POSTGRES_USERNAME}" \
+        -c "\dt" | grep "3 rows"
+
+    # # Generate OV
+    # greenprint "🔧 Generate OV"
+    # sudo /usr/libexec/fdo/fdo-manufacturing-client no-plain-di --insecure --manufacturing-server-url "http://${FDO_SERVER_ADDRESS}:8080"
+
+    # greenprint "🔧 Check manufacturing server db for new OV"
+    # sudo podman exec \
+    #     postgres \
+    #     psql \
+    #     --username="${POSTGRES_USERNAME}" \
+    #     -c "SELECT * FROM manufacturer_vouchers ;" | grep "1 row"
+
+    # greenprint "🔧 Export OV"
+    # mkdir -p "${TEMPDIR}/export-ov"
+    # /usr/libexec/fdo/fdo-owner-tool export-manufacturer-vouchers postgres "postgresql://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_IP}/${POSTGRES_DB}" "${TEMPDIR}/export-ov/"
+
+    # greenprint "🔧 Import OV into owner db"
+    # EXPORTED_FILE=$(ls -1 "${TEMPDIR}/export-ov")
+    # /usr/libexec/fdo/fdo-owner-tool import-ownership-vouchers postgres "postgresql://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_IP}/${POSTGRES_DB}" "${TEMPDIR}/export-ov/${EXPORTED_FILE}"
+
+    # greenprint "🔧 Check owner db for imported OV"
+    # sudo podman exec \
+    #     postgres \
+    #     psql \
+    #     --username="${POSTGRES_USERNAME}" \
+    #     -c "SELECT * FROM owner_vouchers ;" | grep "1 row"
+
+    # greenprint "🔧 Sleep 60 seconds to sync with rendezvous db"
+    # sleep 60
+
+    # greenprint "🔧 Check rendezvous db for synced OV"
+    # sudo podman exec \
+    #     postgres \
+    #     psql \
+    #     --username="${POSTGRES_USERNAME}" \
+    #     -c "SELECT * FROM rendezvous_vouchers ;" | grep "1 row"
+fi
 
 # Compare rpm package version
 function nvrGreaterOrEqual {
@@ -434,22 +582,6 @@ sudo rm -rf "$PROD_REPO"
 sudo mkdir -p "$PROD_REPO"
 sudo ostree --repo="$PROD_REPO" init --mode=archive
 sudo ostree --repo="$PROD_REPO" remote add --no-gpg-verify edge-stage "$STAGE_REPO_URL"
-
-# Prepare stage repo network
-greenprint "🔧 Prepare stage repo network"
-sudo podman network inspect edge >/dev/null 2>&1 || sudo podman network create --driver=bridge --subnet=192.168.200.0/24 --gateway=192.168.200.254 edge
-
-# Clear container running env
-greenprint "🧹 Clearing container running env"
-# Remove any status containers if exist
-sudo podman ps -a -q --format "{{.ID}}" | sudo xargs --no-run-if-empty podman rm -f
-# Remove all images
-sudo podman rmi -f -a
-
-# Wait for fdo server to be running
-until [ "$(curl -X POST http://${FDO_SERVER_ADDRESS}:8080/ping)" == "pong" ]; do
-    sleep 1;
-done;
 
 ##########################################################
 ##
